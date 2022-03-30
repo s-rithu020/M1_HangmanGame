c902c0be5675196f9e4184281b27c6f6d802ef11
s-rithu020 <rithu165@gmail.com>
2022-03-31 01:14:33 +0530

    update

--- 3_Implementation/src/replay.c
+++ 3_Implementation/src/replay.c
@@ -6,7 +6,7 @@ void replay()
     char selected[MAXWORDS];
     while (choice == FALSE) {
         printf("Do you want to play again? (y/n)\n");
-        fscanf("%9999s", &selected[0]);
+        scanf("%s", &selected[0]);
         if (selected[0] == 'y') {
             choice = TRUE;
             startGame();
