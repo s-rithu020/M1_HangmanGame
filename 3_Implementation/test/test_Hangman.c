59fd3cf64f96a42f481641d5cb934961a0bd1313
s-rithu020 <rithu165@gmail.com>
2022-04-03 15:52:46 +0530

    commit

--- 3_Implementation/test/test_Hangman.c
+++ 3_Implementation/test/test_Hangman.c
@@ -3,10 +3,6 @@
 
 //#define PROJECT_NAME "HangmanGame"
 
-//int test_drawHangman();
-//void test_replay(void);
-//void test_startGame(void);
-
 // Required by the unity test framework
 void setUp()
 {
@@ -18,16 +14,9 @@ void tearDown()
 
 }
 
-void test_drawHangman(void)
+void test_start()
 {
-    int mistakes = 0;
-    //TEST_ASSERT_EQUAL(0, drawHangman(0));
-    TEST_ASSERT_EQUAL(0,drawHangman(0));
-    //TEST_ASSERT_EQUAL(2, drawHangman(2));
-    //TEST_ASSERT_EQUAL(3, drawHangman(3));
-    //TEST_ASSERT_EQUAL(4, drawHangman(4));
-    //TEST_ASSERT_EQUAL(5, drawHangman(5));
-    //TEST_ASSERT_EQUAL(6, drawHangman(6));
+    TEST_ASSERT_EQUAL(-1, startGame());
 }
 
 
@@ -38,9 +27,7 @@ int main()
     UNITY_BEGIN();
 
     //Run Test functions
-    RUN_TEST(test_drawHangman);
-    //RUN_TEST(test_replay);
-    //RUN_TEST(test_startGame);
+    RUN_TEST(test_start);
 
     //Close the Unity Test Framework
     return UNITY_END();
